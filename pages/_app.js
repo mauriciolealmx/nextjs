7343c43f58dafc58ea8896a669aab448a5d1002f
Mauricio Leal <mauriciolealmx@gmail.com>
2020-12-08 22:11:52 -0600

    Set amplify

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+
+import Amplify from 'aws-amplify';
+import config from '../aws-exports';
+
+Amplify.configure({
+  ...config,
+  ssr: true,
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
