b08af8f15fc5bfa98c3366365741977c37599739
Mauricio Leal <mauriciolealmx@gmail.com>
2020-12-13 11:53:26 -0600

    Update app data

--- components/layout.js
+++ components/layout.js
@@ -3,8 +3,8 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Your Name';
-export const siteTitle = 'Next.js Sample Website';
+const name = 'Five good reasons why';
+export const siteTitle = 'Five good reasons why';
 
 export default function Layout({ children, home }) {
   return (
@@ -13,7 +13,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Should you or should you not? Find five good reasons why."
         />
         <meta
           property="og:image"
--- pages/index.js
+++ pages/index.js
@@ -29,15 +29,8 @@ export default function Home({ allPosts }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Top Five Blogs</h2>
+        <h2 className={utilStyles.headingLg}>You should:</h2>
         <ul className={utilStyles.list}>
           {posts.length > 0 &&
             posts?.map((post) => (
