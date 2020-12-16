0e8edca78cdeeb54c799a846a63d88d0d7e3671a
Mauricio Leal <mauricio.leal@avigilon.com>
2020-12-16 17:47:33 -0600

    Add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+import * as gtag from '../lib/gtag';
 
 import Amplify from 'aws-amplify';
 import config from '../aws-exports';
+
+import '../styles/globals.css';
 
 Amplify.configure({
   ...config,
@@ -9,5 +14,18 @@ Amplify.configure({
 });
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
