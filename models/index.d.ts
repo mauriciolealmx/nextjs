97025ab7bd106645db40b5dc9423fe019a2c8543
Mauricio Leal <mauriciolealmx@gmail.com>
2020-12-17 21:43:35 -0600

    Add createdAt to graphql schema

--- models/index.d.ts
+++ models/index.d.ts
@@ -8,6 +8,7 @@ export declare class Post {
   readonly id: string;
   readonly title?: string;
   readonly content?: string;
+  readonly createdAt?: string;
   constructor(init: ModelInit<Post>);
   static copyOf(source: Post, mutator: (draft: MutableModel<Post>) => MutableModel<Post> | void): Post;
 }
