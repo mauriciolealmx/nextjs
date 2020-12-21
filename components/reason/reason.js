4c42b4d0187ec4cbe20d068660b77977d27e5f60
Mauricio Leal <mauriciolealmx@gmail.com>
2020-12-20 18:42:57 -0600

    Add voting UI

--- components/reason/reason.js
+++ components/reason/reason.js
@@ -1,8 +1,19 @@
+import UpTriange from '../../svgs/upTriange.svg';
+
 import styles from './reason.module.css';
 
 export default function Reason({ index, reason }) {
   return (
     <li className={styles.reasonRoot}>
+      <div className={styles.votingRoot}>
+        <button className={styles.textButton}>
+          <UpTriange className={styles.btVote} />
+        </button>
+        <span>{reason.votes}</span>
+        <button className={`${styles.textButton} ${styles.rotated}`}>
+          <UpTriange className={styles.btVote} />
+        </button>
+      </div>
       <span className={styles.marker}>{index}.</span>
       {reason.title}
     </li>
---
+++ svgs/upTriange.svg.js
@@ -0,0 +1,7 @@
+export default function UpTriange({ className }) {
+  return (
+    <svg width="20" height="36" viewBox="0 0 36 36" className={className}>
+      <path d="M2 26h32L18 10 2 26z"></path>
+    </svg>
+  );
+}
