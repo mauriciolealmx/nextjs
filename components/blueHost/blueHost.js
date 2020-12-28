466be5210ca81c96317d93edad0a13bae02463e1
Mauricio Leal <mauriciolealmx@gmail.com>
2020-12-27 21:17:14 -0600

    Add affiliate gtag event

--- components/blueHost/blueHost.js
+++ components/blueHost/blueHost.js
@@ -1,10 +1,23 @@
+import * as gtag from '../../lib/gtag';
+
 const TRACKING_URL = process.env.NEXT_PUBLIC_BH_TRACKING_URL;
 const IMAGE_URL = process.env.NEXT_PUBLIC_BH_IMG_URL;
 
-const BlueHost = () => (
-  <a href={TRACKING_URL} target="_blank">
-    <img border="0" src={IMAGE_URL} />
-  </a>
-);
+const BlueHost = () => {
+  const handleClick = () => {
+    gtag.event({
+      action: 'click',
+      category: 'affiliate',
+      label: 'click_affiliate',
+      value: 'bluehost',
+    });
+  };
+
+  return (
+    <a href={TRACKING_URL} target="_blank" onClick={handleClick}>
+      <img border="0" src={IMAGE_URL} />
+    </a>
+  );
+};
 
 export default BlueHost;
