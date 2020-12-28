2bfed4a5d987551c443eeb775edef4ce2a785697
Mauricio Leal <mauriciolealmx@gmail.com>
2020-12-28 12:20:57 -0600

    Add fiverr post and urls

---
+++ components/fiverr/fiverr.js
@@ -0,0 +1,12 @@
+const TRACKING_URL = process.env.NEXT_PUBLIC_FIVERR_TRACKING_URL;
+const IMAGE_URL = process.env.NEXT_PUBLIC_FIVERR_IMG_URL;
+
+const Fiverr = () => {
+  return (
+    <a href={TRACKING_URL} target="_Top">
+      <img border="0" src={IMAGE_URL} width="340" height="70" />
+    </a>
+  );
+};
+
+export default Fiverr;
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -6,6 +6,8 @@ import { useRouter } from 'next/router';
 import { withSSRContext } from 'aws-amplify';
 
 import BlueHost from '../../components/blueHost/blueHost';
+import Fiverr from '../../components/fiverr/fiverr';
+
 import Date from '../../components/date';
 import Layout from '../../components/layout';
 import Reason from '../../components/reason/reason';
@@ -14,9 +16,7 @@ import { Post } from '../../models';
 import utilStyles from '../../styles/utils.module.css';
 
 const blueHostId = '0008c2c3-67c3-4df3-8ead-535d6f577650';
-const linkUrl = 'https://www.bluehost.com/track/mauricioleal/googleAds';
-const imgSrc =
-  'https://bluehost-cdn.com/media/partner/images/mauricioleal/728x90/728x90BW.png';
+const fiverrId = '2741a20e-3687-4113-a6a1-7004bc8fc5fa';
 
 const fixReasons = [
   {
@@ -57,6 +57,7 @@ export default function PostComp({ post }) {
   }
 
   const isBluehost = postState.id === blueHostId;
+  const isFiverr = postState.id === fiverrId;
 
   // TODO: reasonTitle should be id
   const handleVote = useCallback(async (voteValue, title) => {
@@ -106,6 +107,7 @@ export default function PostComp({ post }) {
         )}
       </article>
       {isBluehost && <BlueHost />}
+      {isFiverr && <Fiverr />}
     </Layout>
   );
 }
