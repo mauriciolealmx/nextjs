6989861ac624921a7c6a81137d79a11a786f31da
Mauricio Leal <mauriciolealmx@gmail.com>
2021-01-06 18:35:04 -0600

    Add absolute imports support

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -5,15 +5,15 @@ import { DataStore } from '@aws-amplify/datastore';
 import { useRouter } from 'next/router';
 import { withSSRContext } from 'aws-amplify';
 
-import BlueHost from '../../components/blueHost/blueHost';
-import Fiverr from '../../components/fiverr/fiverr';
+import BlueHost from 'components/blueHost/blueHost';
+import Fiverr from 'components/fiverr/fiverr';
 
 import Date from 'components/date';
-import Layout from '../../components/layout';
-import Reason from '../../components/reason/reason';
-import { Post, ReasonV2 } from '../../models';
+import Layout from 'components/layout';
+import Reason from 'components/reason/reason';
+import { Post, ReasonV2 } from 'models';
 
-import utilStyles from '../../styles/utils.module.css';
+import utilStyles from 'styles/utils.module.css';
 
 const blueHostId = '0008c2c3-67c3-4df3-8ead-535d6f577650';
 const fiverrId = '2741a20e-3687-4113-a6a1-7004bc8fc5fa';
